7c5515f1ebfc6f3e234ed636f9d9ea1d1c689d4e
Administrator <Administrator@PMT2031>
2025-11-19 14:50:12 +0700

    XONG BUOI 1

--- nmlt/buoi1/buoi1.cpp
+++ nmlt/buoi1/buoi1.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
+XONG BUOI 1
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
