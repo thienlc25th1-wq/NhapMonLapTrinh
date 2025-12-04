262e64126693ac7129b9ecc791e5f4c9dbb61e6d
buooi4 <Administrator@PMT2031>
2025-12-04 11:15:25 +0700

    bioi 5

--- nmlt/buoi5/buoi5.cpp
+++ nmlt/buoi5/buoi5.cpp
@@ -11,10 +11,18 @@ void cau1() {
 	if (c > a && c > b)
 		cout << c;
  }
-
+void cau2() {
+	float a, b;
+	cout << " Nhap a,b: ";
+	cin >> a >> b;
+	if (a * b > 0)
+		cout << "cung dau";
+	else
+		cout << "Khac dau";
+}
 int main()
 {
-	cau1();
+	cau2();
 }
 
 
