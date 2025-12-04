e4a8588b175e5f8017e34cb8d53715dc05c3050e
buooi4 <Administrator@PMT2031>
2025-12-04 09:46:17 +0700

    buoi4

--- nmlt/buoi4/buoi4.cpp
+++ nmlt/buoi4/buoi4.cpp
@@ -6,9 +6,9 @@ void cau1() {
 	string hoten;
 	int namsinh;
 	bool gioitinh;
-	cout << "Nhao ho ten: ";
+	cout << "Nhap ho ten: ";
 	getline(cin, hoten);
-	cout << " Nha nam sinh: ";
+	cout << " Nhap nam sinh: ";
 	cin >> namsinh;
 	cout << "Nhap gioi tinh (1 - Nam; 0 - Nu): ";
 	int temp;
@@ -16,8 +16,34 @@ void cau1() {
 	gioitinh = temp == 1 ? true : false;
 	cout << hoten << "\t" << namsinh << "\t" << (gioitinh == true ? " Nam" : "Nu") << endl;
 }
+void cau2() {
+int sotien;
+cout << "Nhap so tien: ";
+cin >> sotien;
+int to500k = sotien / 500000;
+cout << " so to 500k: " << to500k << endl;
+sotien -= to500k * 500000;
+int to200k = sotien / 200000;
+cout << " so to 200k: " << to200k << endl;
+sotien -= to200k * 200000;
+int to100k = sotien / 100000;
+cout << " so to 100k: " << to100k << endl;
+sotien -= to100k * 100000;
+int to50k = sotien / 50000;
+cout << " so to 50k: " << to50k << endl;
+sotien -= to50k * 50000;
+int to20k = sotien / 20000;
+cout << " so to 20k: " << to20k << endl;
+sotien -= to20k * 20000;
+int to10k = sotien / 10000;
+cout << " so to 10k: " << to10k << endl;
+sotien -= to10k * 10000;
+}
+
+
+
 
 int main()
 {
-	cau1();
+	cau2();
 }
