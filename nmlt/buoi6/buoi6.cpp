ab12861d12c0449bee6f375817ce5f29bfd979fa
Administrator <Administrator@PMT2031>
2025-12-11 15:00:41 +0700

    buoi6

--- nmlt/buoi6/buoi6.cpp
+++ nmlt/buoi6/buoi6.cpp
@@ -36,10 +36,41 @@ for (int i = 1; i <= 10; i++) {
 	cout << n << "x" << i << "=" << n * i << endl;
 }
 }
-
-
+void cau5() {
+int n;
+cout << " Nhap n: ";
+cin >> n;
+for (int i = 1; i <= n; i++) {
+	if (n % i == 0) {
+		cout << i << endl;
+	}
+}
+void cau6() {
+	int n, tong = 0;
+	cout << "Nhap n: ";
+	cin >> n;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				tong += i;
+			}
+		}
+	cout << "Tong cac uoc: " << tong << endl;
+}
+void cau7();
+int n, tong = 0;
+cout << " nhap n: ";
+cin >> n;
+for (int i = 1; i < n; i++) {
+	if (n % i == 0) {
+		tong += i;
+	}
+}
+if (tong == n)
+cout << n << "la so hoan thien" << endl;
+else {
+	cout << n << "ko la so hoan thien" << endl;
 int main()
 {
-	cau4();
+	cau6();
 }
 
