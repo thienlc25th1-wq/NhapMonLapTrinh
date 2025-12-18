8b51b7636bd3ebe711af9fd86e3b392915eb88fa
Administrator <Administrator@PMT2031>
2025-12-18 14:14:37 +0700

    buoi8

--- nmlt/buoi8/Array.cpp
+++ nmlt/buoi8/Array.cpp
@@ -5,3 +5,12 @@ void input(int b[], int & n) {
 	cin >> n;
 
 }
+void output(int b[], int n) {
+	cout << "Danh sach phan tu mang: " << endl;
+	int i = 0; 
+	while (i < n) {
+		cout << b[i] << "  ";
+		i++;
+	}
+	cout << endl;
+}
